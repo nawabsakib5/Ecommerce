907e4c8a1a9d3d336c2499fd631bed3cd8a4bedd
nawabsakib5 <nawabsakib5@gmail.com>
2026-03-06 03:06:28 +0600

    Add signup page

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -7,8 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , index , name= 'index'),
-    path('contact/' , contact , name= 'contact'),
+    path('' , include ('core.urls')),
     path('items/' , include('item.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
