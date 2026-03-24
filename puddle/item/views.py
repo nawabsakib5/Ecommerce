4895bc6db4a529a3388305c479b2267f3279f2d8
Mohammad Sakib <nawabsakib5@gmail.com>
2026-03-25 02:17:49 +0600

    today's update

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from django.contrib.auth.decorators import login_required
-from .forms import NewItemForm
+from .forms import *
+
+def items(request):
+    query = request.GET.get('query', '')
+    items = Item.objects.filter(is_sold=False)
+
+
+    if query:
+        items = items.filter(name__icontains=query)
+
+    return render(request, 'item/items.html', {
+        'items':items,
+        'query':query,
+    })
+
+
 
 def detail(request, pk):
 
@@ -34,3 +49,32 @@ def new(request):
         'form' : form,
         'title' : 'New item',
     })
+
+
+@login_required
+def edit(request , pk):
+    item = get_object_or_404(Item ,pk =pk, user= request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect ('item:detail', pk=item.id )
+        
+    else:
+        form = EditItemForm(instance=item)
+
+
+    return render(request, 'item/form.html', {
+        'form' : form,
+        'title' : 'Edit item',
+    })
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item ,pk =pk, user= request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
