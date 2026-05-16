752e1323aa1f19337bff80972c64c75b372548d2
nawabsakib5 <nawabsakib5@gmail.com>
2026-05-17 02:11:03 +0600

    Fix Case Sensitivity for Payment app and Optimized Queries

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
-    path('payment/', include('payment.urls')), 
+    path('payment/', include('Payment.urls')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
