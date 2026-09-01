46ba6dcaf6b65fbb6dfa17e673e264c86fef18ba
nawabsakib5 <nawabsakib5@gmail.com>
2026-09-02 01:04:52 +0600

    Database Change

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,12 +1,9 @@
-
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
     path('cado-secure-mgmt-2026/', admin.site.urls),
     path('', include('core.urls')),
     path('items/', include('item.urls')),
@@ -14,4 +11,5 @@ urlpatterns = [
     path('inbox/', include('conversation.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
