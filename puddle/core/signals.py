302966989ec7237f3222f2ad98bd19650c394158
nawabsakib5 <nawabsakib5@gmail.com>
2026-09-03 00:55:39 +0600

    Goggle Oauth Integrations

--- puddle/core/signals.py
+++ puddle/core/signals.py
@@ -1,16 +1,13 @@
-from allauth.socialaccount.signals import social_account_added, pre_social_login
+from allauth.socialaccount.signals import pre_social_login
 from django.dispatch import receiver
-from allauth.socialaccount.models import SocialLogin
 
 
 @receiver(pre_social_login)
 def set_user_type_on_social_login(sender, request, sociallogin, **kwargs):
-    """Google দিয়ে নতুন user signup হলে Buyer set করো"""
+    user = sociallogin.user
     if sociallogin.is_new:
-        user = sociallogin.user
-        if not user.pk:
+        user.user_type = 'Buyer'
+    elif user.pk:
+        if not user.user_type:
             user.user_type = 'Buyer'
-        else:
-            if not user.user_type:
-                user.user_type = 'Buyer'
-                user.save(update_fields=['user_type'])
+            user.save(update_fields=['user_type'])
